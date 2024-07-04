b0772703352b084db2b7c784e77761779caa47e0
gauravphuyal <phuyalgaurav90@gmail.com>
2024-07-04 18:35:45 +0545

    Add Guessing Functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess the Number!");
     println!("Please input your guess.");
 
     let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..2);
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to readline");
+
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Number is needed for the guess");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small !"),
+        Ordering::Equal => println!("You Won !"),
+        Ordering::Greater => println!("Too Big !"),
+    }
 }
