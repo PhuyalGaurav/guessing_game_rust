34f281d8246c4e27a533b0b5b0b8a18cc71a1916
gauravphuyal <phuyalgaurav90@gmail.com>
2024-07-04 18:48:58 +0545

    refactor: Improve guessing functionality and input validation

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,28 @@ use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Guess the Number!");
-    println!("Please input your guess.");
-
     let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..2);
+    let secret_number = rand::thread_rng().gen_range(1..100);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to readline");
+    loop {
+        guess.clear();
+        println!("Please Input Your Guess! : ");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to readline");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Number is needed for the guess");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small !"),
-        Ordering::Equal => println!("You Won !"),
-        Ordering::Greater => println!("Too Big !"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small !"),
+            Ordering::Equal => {
+                println!("You Won!! ");
+                break;
+            }
+            Ordering::Greater => println!("Too Big !"),
+        }
     }
 }
